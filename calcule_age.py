761c01e268cbd86a7c91d5ae82614050ac7b03c5
Gabriel Augusto <gab.augusto@gmail.com>
2020-09-16 12:39:31 -0300

    Update calcule_age.py

--- calcule_age.py
+++ calcule_age.py
@@ -2,13 +2,13 @@ def find_age(current_day, current_month, current_year, birth_day, birth_month, b
     month = [31,28,31,30,31,30,31,31,30,31,30,31]
     if (birth_day > current_day):
         current_month -= 1
-        current_day = current_date + month[birth_month-1]
+        current_day = current_day + month[birth_month-1]
         
     if (birth_month > current_month):
         current_year -=  1
         current_month += 12
         
-    calculated_day = current_date - birth_day
+    calculated_day = current_day - birth_day
     calculated_month = current_month - birth_month
     calculated_year = current_year - birth_year
     
